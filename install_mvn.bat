fb7a1c12c5d06b21dcf750a582ff60413d9618bb
Kirill "Zkir <mekillot@yandex.ru>
2025-09-05 02:48:29 +0300

    Create install_mvn.bat

---
+++ install_mvn.bat
@@ -0,0 +1,7 @@
+mvn install:install-file ^
+   -Dfile=d:\src\Essentials\jars\EssentialsX-2.21.0-dev+185-5bf158c.jar ^
+   -DgroupId=net.essentialsx ^
+   -DartifactId=EssentialsX ^
+   -Dversion=2.21.0-ZKIR-SNAPSHOT ^
+   -Dpackaging=jar ^
+   -DgeneratePom=true
