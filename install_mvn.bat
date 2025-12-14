9b42bb366fa340c2a350eb9361b558d32981611c
Kirill B. aka Zkir <zkir@zkir.ru>
2025-12-14 22:54:06 +0300

    Update install_mvn.bat

--- install_mvn.bat
+++ install_mvn.bat
@@ -1,7 +1,7 @@
 mvn install:install-file ^
-   -Dfile=d:\src\Essentials\jars\EssentialsX-2.21.0-dev+185-5bf158c.jar ^
+   -Dfile=d:\src\Essentials\jars\EssentialsX-2.22.0-dev+188-fb7a1c1.jar ^
    -DgroupId=net.essentialsx ^
    -DartifactId=EssentialsX ^
-   -Dversion=2.21.0-ZKIR-SNAPSHOT ^
+   -Dversion=2.22.0-ZKIR-SNAPSHOT ^
    -Dpackaging=jar ^
    -DgeneratePom=true
